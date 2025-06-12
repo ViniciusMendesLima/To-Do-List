24e418ee8e2e463faa79e0e1e4d3fbac0e7e53a2
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-06-11 21:50:53 -0300

    adicionado pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Todo from "./components/Todo";
 
 import "./App.css";
 import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -26,6 +27,8 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const [search, setSearch] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -60,15 +63,20 @@ function App() {
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            removeTodo={removeTodo}
-            completeTodo={completeTodo}
-          />
-        ))}
+        {todos
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
